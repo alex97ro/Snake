c537e427db0c22fa89ea3656f8d8983f8dacc718
alex97ro <timoftialex@yahoo.com>
2016-12-13 14:37:24 +0200

    meniu joc

--- main.cpp
+++ main.cpp
@@ -1,9 +1,73 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <conio.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    char joc_nou[10]="JOC NOU",tabel[10]="TABEL",iesire[10]="IESIRE";
+    cout<<"  >"<<joc_nou<<"<  "<<endl;
+    cout<<"   "<<tabel<<"   "<<endl;
+    cout<<"   "<<iesire<<"   ";
+    unsigned short pozitie=0;
+cout<<getch();
+cout<<endl;
+while(1){
+        switch (getch())
+            {
+           case 119:
+            if(pozitie==0)
+            {
+                system("cls");
+                cout<<"   "<<joc_nou<<"   "<<endl;
+                cout<<"   "<<tabel<<"   "<<endl;
+                cout<<"  >"<<iesire<<"<  ";
+                pozitie=2;
+            }
+            else if(pozitie==1)
+            {
+                system("cls");
+                cout<<"  >"<<joc_nou<<"<  "<<endl;
+                cout<<"   "<<tabel<<"   "<<endl;
+                cout<<"   "<<iesire<<"   ";
+                pozitie=0;
+            }
+           else if(pozitie==2)
+            {
+                system("cls");
+                cout<<"   "<<joc_nou<<"   "<<endl;
+                cout<<"  >"<<tabel<<"<  "<<endl;
+                cout<<"   "<<iesire<<"   ";
+                pozitie=1;
+            }
+            break;
+           case 115:
+            if(pozitie==0)
+            {
+                system("cls");
+                cout<<"   "<<joc_nou<<"   "<<endl;
+                cout<<"  >"<<tabel<<"<  "<<endl;
+                cout<<"   "<<iesire<<"   ";
+                pozitie=1;
+            }
+        else if(pozitie==1)
+            {
+                system("cls");
+                cout<<"   "<<joc_nou<<"   "<<endl;
+                cout<<"   "<<tabel<<"   "<<endl;
+                cout<<"  >"<<iesire<<"<  ";
+                pozitie=2;
+            }
+            else if(pozitie==2)
+            {
+                system("cls");
+                cout<<"  >"<<joc_nou<<"<  "<<endl;
+                cout<<"   "<<tabel<<"   "<<endl;
+                cout<<"   "<<iesire<<"   ";
+                pozitie=0;
+            }
+            break;
+    }
+}
+
 }
