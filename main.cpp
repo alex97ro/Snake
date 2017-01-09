f8585d16cebab026bb59ea5a745e74281d2eb491
alex97ro <timoftialex@yahoo.com>
2017-01-09 21:23:26 +0200

    am modificat main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 #include <conio.h>
 #include <windows.h>
 using namespace std;
+struct sarpe
+{
+    int x,y;
 
+}componente[100];
 char harta[100][100];
-int n=30;
+int n=20,lungime_sarpe;
 void harta_init()
 {
     int i,j;
@@ -20,6 +24,39 @@ void harta_init()
     for(i=2;i<n;i++)
         for(j=2;j<n;j++)
        harta[i][j]=' ';
+
+       harta[4][4]='o';
+       harta[4][5]='o';
+       harta[4][6]='o';
+
+       lungime_sarpe=3;
+
+       componente[1].x=4;
+       componente[1].y=4;
+       componente[2].x=4;
+       componente[2].y=5;
+       componente[3].x=4;
+       componente[3].y=6;
+}
+void muta_componentele(int x,int y)
+{
+    int i;
+    for(i=n;i>1;i--)
+    {
+        componente[i].x=componente[i-1].x;
+        componente[i].y=componente[i-1].y;
+    }
+    componente[1].x=x;
+    componente[1].y=y;
+}
+void harta_update()
+{
+    int i,j;
+    for(i=2;i<n;i++)
+        for(j=2;j<n;j++)
+       harta[i][j]=' ';
+       for(i=1;i<=lungime_sarpe;i++)
+        harta[componente[i].x][componente[i].y]='o';
 }
 void harta_afisare()
 {
@@ -32,16 +69,10 @@ void harta_afisare()
     }
 
 }
-struct sarpe
-        {
-            unsigned short x,y;
-            sarpe *urm;
-        }*cap;
-
 int main()
 {
     char joc_nou[10]="JOC NOU",tabel[10]="TABEL",iesire[10]="IESIRE";
-    unsigned pozitie=0;
+    unsigned short pozitie=0,directie=0;
     bool joc_in_desfasurare;
     cout<<"  >"<<joc_nou<<"<  "<<endl;
     cout<<"   "<<tabel<<"   "<<endl;
